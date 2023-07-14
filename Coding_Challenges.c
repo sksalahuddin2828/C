bb6125a37aa58f0b7b4971cbdb69f040ff36f620
SK. SALAHUDDIN <127140921+sksalahuddin2828@users.noreply.github.com>
2023-07-14 15:26:46 +0600

    Update Coding_Challenges.c

--- Coding_Challenges.c
+++ Coding_Challenges.c
@@ -57,7 +57,28 @@ int main() {
 
 //----------------------------------------------------------------------------------------------------------------------------------------------
 
-// 3. 
+// 3. Printing the Fibonacci Series using Recursion:
+
+#include <stdio.h>
+
+int fibonacci(int n) {
+    if (n <= 1) {
+        return n;
+    } else {
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+}
+
+int main() {
+    int n = 10;
+    printf("Fibonacci series: ");
+    
+    for (int i = 0; i < n; i++) {
+        printf("%d ", fibonacci(i));
+    }
+    
+    return 0;
+}
 
 //----------------------------------------------------------------------------------------------------------------------------------------------
 
