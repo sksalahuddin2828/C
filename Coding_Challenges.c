9f73d19a8bbc1dfe659b4f585e8e9da676bc5798
SK. SALAHUDDIN <127140921+sksalahuddin2828@users.noreply.github.com>
2023-07-14 15:27:43 +0600

    Update Coding_Challenges.c

--- Coding_Challenges.c
+++ Coding_Challenges.c
@@ -103,4 +103,21 @@ int main() {
 
 //----------------------------------------------------------------------------------------------------------------------------------------------
 
-// 5. 
+// 5. Finding the Average of Numbers:
+
+#include <stdio.h>
+
+int main() {
+    int numbers[] = {10, 20, 30, 40, 50};
+    int size = sizeof(numbers) / sizeof(numbers[0]);
+    int sum = 0;
+    
+    for (int i = 0; i < size; i++) {
+        sum += numbers[i];
+    }
+    
+    float average = (float)sum / size;
+    printf("Average: %.2f", average);
+    
+    return 0;
+}
