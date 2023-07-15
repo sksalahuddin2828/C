cd2f4cef8ee2c1832081cf0f18d171e83735d24a
SK. SALAHUDDIN <127140921+sksalahuddin2828@users.noreply.github.com>
2023-07-15 13:36:30 +0600

    Create armstrong_number.c

---
+++ armstrong_number.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <math.h>
+
+int is_armstrong_number(int n) {
+    int num_digits = 0;
+    int temp = n;
+    while (temp > 0) {
+        temp /= 10;
+        num_digits++;
+    }
+    int sum_of_powers = 0;
+    temp = n;
+    while (temp > 0) {
+        int digit = temp % 10;
+        sum_of_powers += pow(digit, num_digits);
+        temp /= 10;
+    }
+    return n == sum_of_powers;
+}
+
+int main() {
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    if (is_armstrong_number(num)) {
+        printf("%d is an Armstrong number.\n", num);
+    } else {
+        printf("%d is not an Armstrong number.\n", num);
+    }
+    return 0;
+}
