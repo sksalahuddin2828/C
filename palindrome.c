24b8791669c8dbfdba29a9ccfcb47bdfdc0f284b
SK. SALAHUDDIN <127140921+sksalahuddin2828@users.noreply.github.com>
2023-07-15 13:49:19 +0600

    Update palindrome.c

--- palindrome.c
+++ palindrome.c
@@ -32,3 +32,35 @@ int main() {
     }
     return 0;
 }
+
+//----------------------------------------------------------------------------------------
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <ctype.h>
+#include <string.h>
+
+bool is_palindrome(char *s) {
+    int length = strlen(s);
+    char filtered[length];
+    int index = 0;
+    for (int i = 0; i < length; i++) {
+        if (isalnum(s[i])) {
+            filtered[index++] = tolower(s[i]);
+        }
+    }
+    for (int i = 0, j = index - 1; i < j; i++, j--) {
+        if (filtered[i] != filtered[j]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main() {
+    char str[] = "A man, a plan, a canal: Panama";
+    printf("%s\n", is_palindrome(str) ? "True" : "False");  
+    return 0;
+}
+
+// Output: True
