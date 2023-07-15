9b1fe27e2147e33e001ed0c3c61ed72116366687
SK. SALAHUDDIN <127140921+sksalahuddin2828@users.noreply.github.com>
2023-07-15 14:04:17 +0600

    Update armstrong_number.c

--- armstrong_number.c
+++ armstrong_number.c
@@ -29,3 +29,32 @@ int main() {
     }
     return 0;
 }
+
+//------------------------------------------------------------------------
+
+#include <stdio.h>
+#include <math.h>
+
+int is_armstrong_number(int n) {
+    int num_digits = 0;
+    int temp = n;
+    while (temp > 0) {
+        temp /= 10;
+        num_digits++;
+    }
+    int sum_of_powers = 0;
+    temp = n;
+    while (temp > 0) {
+        int digit = temp % 10;
+        sum_of_powers += pow(digit, num_digits);
+        temp /= 10;
+    }
+    return n == sum_of_powers;
+}
+
+int main() {
+    printf("%s\n", is_armstrong_number(153) ? "True" : "False"); 
+    return 0;
+}
+
+// Output: True
